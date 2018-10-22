5f19b6743dd9b5931cce057a9d86a9057537719e
Zurc <cruzjurado@gmail.com>
2018-10-22 22:33:21 +0100

    change font family

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,6 +28,7 @@ const Layout = ({ children }) => (
           ]}
         >
           <html lang="en" />
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"></link>
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <Menu />
